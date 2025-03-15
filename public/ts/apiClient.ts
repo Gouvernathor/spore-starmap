021c312c98e0e787525e88eb97ebd321be9aad61
Asriel Senna <aanonyme.personne@hotmail.fr>
2025-03-15 17:28:51 +0100

    Add the api client

---
+++ public/ts/apiClient.ts
@@ -0,0 +1,41 @@
+export class APIClient {
+    constructor(
+        public readonly baseURL: string,
+    ) {}
+
+    async get(endpoint: string) {
+        return (await fetch(this.baseURL + endpoint)).json();
+    }
+
+    async post(endpoint: string, data: any) {
+        return (await fetch(this.baseURL + endpoint, {
+            mode: "cors",
+            method: "POST",
+            body: data,
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })).json();
+    }
+
+    async put(endpoint: string, data: any) {
+        return (await fetch(this.baseURL + endpoint, {
+            mode: "cors",
+            method: "PUT",
+            body: data,
+            headers: {
+                "Content-Type": "application/json",
+            }
+        })).json();
+    }
+
+    async delete(endpoint: string) {
+        return (await fetch(this.baseURL + endpoint, {
+            method: "DELETE",
+        })).json();
+    }
+}
+
+const API_URL = "https://zackbox.app/api/starmap/";
+const client = new APIClient(API_URL);
+export default client;
